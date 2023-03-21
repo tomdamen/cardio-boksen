d0887504b9c1e94ce6bbd0e0f9a3ef023b8db341
Tom Damen <twjdamen@gmail.com>
2023-03-21 20:20:57 +0100

    initial

--- database/seeders/TrainingSeeder.php
+++ database/seeders/TrainingSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Training;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class TrainingSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class TrainingSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Training::factory(5)->create();
     }
 }
